2e20af0a97a2360ebd3e7cf67fc64108d4fea331
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-10 14:31:11 -0500

    Creacion de clase test

---
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,28 @@
+package edu.eci.cvds.tdd.library;
+
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.loan.Loan;
+import edu.eci.cvds.tdd.library.user.User;
+
+public class LibraryTest {
+	
+	public boolean ShouldAddBook(Book book) {
+        //TODO Implement the logic to add a new book into the map.
+        return false;
+    }
+	
+	public Loan loanABook(String userId, String isbn) {
+        //TODO Implement the login of loan a book to a user based on the UserId and the isbn.
+        return null;
+    }
+	
+	public Loan returnLoan(Loan loan) {
+        //TODO Implement the login of loan a book to a user based on the UserId and the isbn.
+        return null;
+    }
+	
+	public boolean addUser(User user) {
+        return false;
+    }
+
+}
--- Library/src/test/java/edu/eci/cvds/tdd/library/Prueba.java
+++
@@ -1,7 +0,0 @@
-package edu.eci.cvds.tdd.library;
-
-public class Prueba {
-	public static void main(String[] args) {
-        System.out.println("Hola");
-    }
-}
