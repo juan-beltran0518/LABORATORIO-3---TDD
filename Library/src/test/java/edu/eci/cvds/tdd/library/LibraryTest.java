6e8392685ccf3e916d2befd9d90bd44321d05fd3
juan-beltran0518 <juan.brodriguez@mail.escuelaing.edu.co>
2025-02-12 15:29:43 -0500

    Prueba en desarrollo shouldNotAllowEmptyUserName, la prueba falla

--- Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -3,14 +3,8 @@ package edu.eci.cvds.tdd.library;
 import edu.eci.cvds.tdd.library.book.Book;
 import edu.eci.cvds.tdd.library.loan.Loan;
 import edu.eci.cvds.tdd.library.user.User;
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
+import edu.eci.cvds.tdd.library.exceptions.InvalidUserException;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 
@@ -41,8 +35,6 @@ public class LibraryTest {
         assertFalse(library.addBook(book2));
     }
 	
-	
-	
 	@Test
     public void ShouldNotAddBookWitNullISBN() {
     	Library library = new Library();
@@ -72,15 +64,23 @@ public class LibraryTest {
 
 	
 	
-	@Test
-	public void shouldNotAllowEmptyUserId() {
-	    User user = new User();
-	    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-	        user.setId(""); // id Vacio 
-	    });
+    @Test
+    public void shouldNotAllowEmptyUserId() {
+        Exception exception = assertThrows(InvalidUserException.class, () -> {
+            new User("Sebastian", ""); 
+        });
 
-	    assertEquals("User ID cannot be empty or null", exception.getMessage());
-	}
+        assertEquals("User ID cannot be empty or null", exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotAllowEmptyUserName() {
+        Exception exception = assertThrows(InvalidUserException.class, () -> {
+            new User("Salomon", ""); 
+        });
+
+        assertEquals("User name cannot be empty or null", exception.getMessage());
+    }
 
 
     
