cacebe2b5f0285d0f2327c266683fd200bc99bd0
juan-beltran0518 <juan.brodriguez@mail.escuelaing.edu.co>
2025-02-12 17:50:07 -0500

    Prueba shouldSetUserNameSuccessfully implementada

--- Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -105,10 +105,19 @@ public class LibraryTest {
         User user = new User("Carlos", "123456");
         assertEquals("123456", user.getId());
     }
+    
+    
         
     public Loan loanABook(String userId, String isbn) {
         //TODO Implement the login of loan a book to a user based on the UserId and the isbn.
         return null;
+    }
+    
+    @Test
+    public void shouldSetUserNameSuccessfully() {
+        User user = new User("Carlos", "123456");
+        user.setName("Alejandro");
+        assertEquals("Alejandro", user.getName());
     }
 
     
